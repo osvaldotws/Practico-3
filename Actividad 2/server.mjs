28d995059b1d404f801fbb96be8aae36110f54f3
Osvaldo Paz <osvaldotws@gmail.com>
2024-10-24 17:15:04 -0300

    Actividad 2

--- Actividad 2/server.mjs
+++ Actividad 2/server.mjs
@@ -3,15 +3,12 @@ import express from 'express'
 const app = express();
 const port = 3000;
 
-
 app.get('/profile', (req, res) => {
     const edad = req.query.edad;
     console.log(`Edad recibida:  ${edad}` );
 
     res.send(`Edad del Perfil : ${edad}`);
 });
-
-
 
 app.listen(port, () =>{
     console.log(`Servidor corriendo en http://localhost:${port}`);
